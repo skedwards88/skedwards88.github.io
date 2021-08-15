27f71748728aea72a6e1251ac38ed2386b8930f1
skedwards88 <skedwards88@github.com>
2021-08-15 13:20:40 -0700

    Updates

--- bundle.js
+++ bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a0f2aef315e52b9628cc";
+/******/ 	var hotCurrentHash = "28bca4e5b9a46f90e411";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
