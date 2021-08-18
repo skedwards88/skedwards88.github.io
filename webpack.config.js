552a3e33362c82d7c9df21a66f6b36b1a91720f7
skedwards88 <skedwards88@github.com>
2021-08-18 08:38:54 -0700

    working with gh-pages except for favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -25,14 +26,27 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "",
     filename: "bundle.js",
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true,
+    publicPath: "http://localhost:3000",
+    hotOnly: false,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [new HtmlWebpackPlugin({
+    // title: 'CnS Games 4',
+    // favicon: "./src/images/favicon.png",
+    // meta: {viewport: 'width=device-width, initial-scale=1'},
+  //   templateContent: `
+  //   <html>
+  //     <body>
+  //       <div id="root"></div>
+  //     </body>
+  //   </html>
+  // `,
+  inject: true,
+  template: "./src/index.html"
+  })],
 };
