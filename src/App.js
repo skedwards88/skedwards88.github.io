70b4ce795f91dd573a1f713bb43b5620aff2df28
skedwards88 <skedwards88@github.com>
2021-08-31 16:42:50 -0700

    wip story

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-import { projects } from "./projects.js";
-import getImageLookup from "./imageLookup.js";
-
-const imageLookup = getImageLookup();
-
-function Project({
-  name,
-  id,
-  description,
-  site,
-  code,
-  activityDescription = "Play now",
-  numPlayers,
-  playTimeMinutes,
-}) {
-  const imagePath = imageLookup[id].default;
-
-  return (
-    <div className="project" key={id}>
-      <h3>{name}</h3>
-      <img className="icon" src={imagePath} alt={id} />
-      <div id="playParameters">
-        <p>{numPlayers ? numPlayers + " players" : null}</p>
-        <p>{numPlayers && playTimeMinutes ? " ● " : null}</p>
-        <p>{playTimeMinutes ? playTimeMinutes + " min" : null}</p>
-      </div>
-      <p>{description}</p>
-      <div className="links">
-        <a href={site}>{activityDescription}</a>
-        <div>&nbsp;●&nbsp;</div>
-        <a href={code}>See code</a>
-      </div>
-    </div>
-  );
-}
 
 function App() {
-
   const startingState = {
     reputation: 10,
     gold: 0,
@@ -66,6 +30,7 @@ function App() {
   const [showConsequence, setShowConsequence] = useState(false);
   const [consequenceText, setConsequenceText] = useState("");
   const [showInventory, setShowInventory] = useState(false);
+  const [currentDisplay, setCurrentDisplay] = useState("location") // location | inventory | consequence
 
   // todo could build programatically...if can remove circular dependency or need test to confirm matches
   const startingItemLocations = {
@@ -82,38 +47,53 @@ function App() {
 
   const locations = {
     room: {
-      description: `You are in a room with a bed. A window faces the west. A wardrobe sits on the north side of the room, opposite a door. ${itemLocations.room.has("lute") ? "A lute leans against the bed. " : ""
-        }${gameState.fire
+      description: `You are in a room with a bed. A window faces the west. A wardrobe sits on the north side of the room, opposite a door. ${
+        itemLocations.room.has("lute") ? "A lute leans against the bed. " : ""
+      }${
+        gameState.fire
           ? "You smell fire and hear screams in the distance. "
           : ""
-        }`,
+      }`,
       connections: ["window", "wardrobe", "inn"],
     },
     window: {
-      description: `${gameState.fire
+      description: `${
+        gameState.fire
           ? "Through the window, you see flames and smoke coming from a nearby mansion. A crowd has gathered in front of the mansion."
           : "Through the window, you see the charred remains of a nearby mansion."
-        }`,
+      }`,
       connections: ["room"],
     },
     wardrobe: {
-      description: `Inside the wardrobe, there is a mirror ${itemLocations.wardrobe.has("clothes") ? "and a set of clothes" : ""
-        }.`,
+      description: `Inside the wardrobe, there is a mirror ${
+        itemLocations.wardrobe.has("clothes") ? "and a set of clothes" : ""
+      }.`,
       connections: ["room", "mirror"],
     },
     mirror: {
       // todo could also handle poopy, singed. Would need to use multiple ternary expressions.
-      description: `${gameState.naked
+      description: `${
+        gameState.naked
           ? "You're naked!"
           : "You are quite good looking, if you say so yourself."
-        }`,
+      }`,
       connections: ["wardrobe"],
     },
     inn: {
-      description: `You enter what appears to be the common room of an inn. ${itemLocations.inn.has("apple") ? "A complementary apple rests on the table. " : ""}${gameState.naked ? 'The inn keeper laughs, "Haven\'t you heard of clothes?!"' : ""}`,
+      description: `You enter what appears to be the common room of an inn. ${
+        itemLocations.inn.has("apple")
+          ? "A complementary apple rests on the table. "
+          : ""
+      }${
+        gameState.naked
+          ? 'The inn keeper laughs, "Haven\'t you heard of clothes?!"'
+          : ""
+      }`,
       connections: ["room", "courtyard"],
       // If you are naked, lose reputation when you move here
-      ... (gameState.naked && { enterSideEffect: { reputation: gameState.reputation - 1 } })
+      ...(gameState.naked && {
+        enterSideEffect: { reputation: gameState.reputation - 1 },
+      }),
     },
     courtyard: {
       description: "A nice fountain",
@@ -121,7 +101,7 @@ function App() {
     },
   };
 
-  const items = {
+  const allItems = {
     lute: {
       spawnLocation: "room",
       description: "A wooden lute",
@@ -131,13 +111,22 @@ function App() {
       }),
       ...(false && { takeDescription: "falsy" }),
       useVerb: "Play",
-      useDescription: "the desc for use",
+      useDescription: "You play a beautiful melody.",
     },
     clothes: {
       spawnLocation: "wardrobe",
       description: "A set of clothes",
-      useVerb: "Wear", // todo not if already on?
-      useDescription: "the desc for use",
+      ...(gameState.naked
+        ? {
+            useVerb: "Wear",
+            useDescription: "You put on the clothes.",
+            useSideEffect: { naked: false },
+          }
+        : {
+            useVerb: "Remove",
+            useDescription: "You strip down.",
+            useSideEffect: { naked: true },
+          }),
     },
     apple: {
       spawnLocation: "inn",
@@ -146,6 +135,18 @@ function App() {
       useDescription: "the desc for use",
     },
     knife: {
+      spawnLocation: "inn",
+      description: "A knife",
+      useVerb: "Attack",
+      useDescription: "the desc for use",
+    },
+    monkey: {
+      spawnLocation: "inn",
+      description: "A knife",
+      useVerb: "Attack",
+      useDescription: "the desc for use",
+    },
+    milk: {
       spawnLocation: "inn",
       description: "A knife",
       useVerb: "Attack",
@@ -160,8 +161,8 @@ function App() {
       (location) => (startingItemLocations[location] = new Set())
     );
 
-    for (const [item, itemInfo] of Object.entries(items)) {
-      startingItemLocations[itemInfo.spawnLocation].add(item)
+    for (const [item, itemInfo] of Object.entries(allItems)) {
+      startingItemLocations[itemInfo.spawnLocation].add(item);
     }
 
     return startingItemLocations;
@@ -175,15 +176,23 @@ function App() {
     setItemLocations(itemLocations);
   }
 
+  function handleMovePlayer(newLocation) {
+    if (locations[newLocation].enterSideEffect) {
+      setGameState({ ...gameState, ...locations[newLocation].enterSideEffect });
+    }
+
+    setPlayerLocation(newLocation);
+  }
+
   function handleTake(item) {
     // Get the "take"" description for the item -- this will be the consequence text
-    const description = items[item].takeDescription
-      ? items[item].takeDescription
+    const description = allItems[item].takeDescription
+      ? allItems[item].takeDescription
       : `You now have ${item}`;
 
     // Get the "take" end location for the item -- will usually be "inventory"
-    const endItemLocation = items[item].takeLocation
-      ? items[item].takeLocation
+    const endItemLocation = allItems[item].takeLocation
+      ? allItems[item].takeLocation
       : "inventory";
 
     // Set the item location to the take end location
@@ -197,38 +206,79 @@ function App() {
   }
 
   function handleUse(item) {
-    const description = items[item].useDescription
-      ? items[item].useDescription
+    const description = allItems[item].useDescription
+      ? allItems[item].useDescription
       : `You use the ${item}.`;
-  }
 
-  function LocationItems({ items }) {
-    return Array.from(items).map((item) => {
-      return (
-        <button onClick={(e) => handleTake(item)} className="item" key={item}>
-          {item}
-        </button>
-      );
-    });
-  }
-
-  function InventoryItems({ items }) {
-    // todo inventory items have different action than take
-    return Array.from(items).map((item) => {
-      return (
-        <button onClick={(e) => handleTake(item)} className="item" key={item}>
-          {item}
-        </button>
-      );
-    });
-  }
-
-  function handleMovePlayer(newLocation) {
-    if (locations[newLocation].enterSideEffect) {
-      setGameState({ ...gameState, ...locations[newLocation].enterSideEffect })
+    if (allItems[item].useSideEffect) {
+      setGameState({ ...gameState, ...allItems[item].useSideEffect });
     }
 
-    setPlayerLocation(newLocation)
+    // set the consequence text to the take description text
+    setConsequenceText(description);
+
+    // set show consequence to true
+    setShowConsequence(true);
+  }
+
+  function handleDrop(item) {
+    // Get the "drop"" description for the item -- this will be the consequence text
+    const description = allItems[item].dropDescription
+      ? allItems[item].dropDescription
+      : `You drop ${item} at ${playerLocation}`;
+
+    // Get the "drop" end location for the item -- will usually be the current player location
+    const endItemLocation = allItems[item].dropLocation
+      ? allItems[item].dropLocation
+      : playerLocation;
+
+    if (allItems[item].dropSideEffect) {
+      setGameState({ ...gameState, ...allItems[item].dropSideEffect });
+    }
+
+    // Set the item location from the inventory to the new location
+    moveItem(item, "inventory", endItemLocation);
+
+    // set the consequence text to the take description text
+    setConsequenceText(description);
+
+    // set show consequence to true
+    setShowConsequence(true);
+  }
+
+  function LocationItems({ itemsAtLocation }) {
+    return Array.from(itemsAtLocation).map((item) => {
+      return (
+        <button onClick={(e) => handleTake(item)} className="item" key={item}>
+          {item}
+        </button>
+      );
+    });
+  }
+
+  function InventoryItems({ itemsInInventory }) {
+    // todo inventory items have different action than take
+    return Array.from(itemsInInventory).map((item) => {
+      return (
+        <div key={item}>
+          <div key={item}>{item}</div>
+          <button
+            onClick={(e) => handleUse(item)}
+            className="item"
+            key={item + "-use"}
+          >
+            {allItems[item].useVerb}
+          </button>
+          <button
+            onClick={(e) => handleDrop(item)}
+            className="item"
+            key={item + "-drop"}
+          >
+            Drop
+          </button>
+        </div>
+      );
+    });
   }
 
   function Connections({ connections }) {
@@ -248,16 +298,15 @@ function App() {
   function Location() {
     return (
       <div className="App">
-        <div className="location">{playerLocation}</div>
         <div className="description">
           {locations[playerLocation]["description"]}
         </div>
-        <LocationItems items={itemLocations[playerLocation]} />
+        <LocationItems itemsAtLocation={itemLocations[playerLocation]} />
         <Connections connections={locations[playerLocation]["connections"]} />
         <button className="inventory" onClick={(e) => setShowInventory(true)}>
           Inventory
         </button>
-        <div>{gameState.reputation}</div>
+        <div>Reputation: {gameState.reputation}</div>
       </div>
     );
   }
@@ -267,7 +316,7 @@ function App() {
       <div className="App">
         <div className="description">{consequenceText}</div>
         <button onClick={(e) => setShowConsequence(false)}>
-          {playerLocation}
+          Back to {playerLocation}
         </button>
       </div>
     );
@@ -276,16 +325,16 @@ function App() {
   function Inventory() {
     return (
       <div className="App">
-        <div className="description">Inventory</div>
-        <InventoryItems items={itemLocations["inventory"]} />
-        <button onClick={(e) => setShowInventory(false)}>Back</button>
+        <div className="description" key="description">
+          Inventory
+        </div>
+        <InventoryItems itemsInInventory={itemLocations["inventory"]} />
+        <button key="back" onClick={(e) => setShowInventory(false)}>
+          Close Inventory
+        </button>
       </div>
     );
   }
-
-  const displays = projects.map((project) => {
-    return Project(project);
-  });
 
   if (showConsequence) {
     return <Consequence></Consequence>;
