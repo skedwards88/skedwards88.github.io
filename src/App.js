cd8478a1b8416a5e880dd8c79eca63774ed434dc
skedwards88 <skedwards88@github.com>
2021-09-01 18:46:44 -0700

    story finish up to baby rescue

--- src/App.js
+++ src/App.js
@@ -45,112 +45,200 @@ function App() {
     courtyard: new Set(["handkerchief"]),
     adolescent: new Set([]),
     fountain: new Set([]),
+    manor: new Set([]),
     nursery: new Set(["baby"]),
+    nurseryWindow: new Set([]),
   };
 
   const [itemLocations, setItemLocations] = useState(startingItemLocations);
 
+  // todo add tests that locations and items have required values
+
+  // todo convert to objects with methods, pass itemLocations/game to remove circular dependency
   const locations = {
     room: {
-      description: `You are in a room with a bed. A window faces the west. A wardrobe sits on the north side of the room, opposite a door. ${itemLocations.room.has("lute") ? "A lute leans against the bed. " : ""
-        }${gameState.fire
+      description: `You are in a room with a bed. A window faces the west. A wardrobe sits on the north side of the room, opposite a door. ${
+        itemLocations.room.has("lute") ? "A lute leans against the bed. " : ""
+      }${
+        gameState.fire
           ? "You smell fire and hear screams in the distance. "
           : ""
-        }`,
+      }`,
       connections: ["window", "wardrobe", "inn"], //todo could say door instead of inn if used alias
       dropProposition: "in", //todo use this when drop. also check that word propositino is correct
     },
     window: {
-      description: `${gameState.fire
-        ? "Through the window, you see flames and smoke coming from a nearby mansion. A crowd has gathered in front of the mansion."
-        : "Through the window, you see the charred remains of a nearby mansion."
-        }`,
+      description: `${
+        gameState.fire
+          ? "Through the window, you see flames and smoke coming from a nearby mansion. A crowd has gathered in front of the mansion."
+          : "Through the window, you see the charred remains of a nearby mansion."
+      }`,
       connections: ["room"],
       dropProposition: "at",
     },
     wardrobe: {
-      description: `Inside the wardrobe, there is a mirror ${itemLocations.wardrobe.has("clothes") ? "and a set of clothes" : ""
-        }.`,
+      description: `Inside the wardrobe, there is a mirror ${
+        itemLocations.wardrobe.has("clothes") ? "and a set of clothes" : ""
+      }.`,
       connections: ["room", "mirror"],
       dropProposition: "in",
     },
     mirror: {
       // todo could also handle poopy, singed. Would need to use multiple ternary expressions.
-      description: `${gameState.naked
-        ? "You're naked!"
-        : "You are quite good looking, if you say so yourself."
-        }`,
+      description: `${
+        gameState.naked
+          ? "You're naked!"
+          : "You are quite good looking, if you say so yourself."
+      }`,
       connections: ["wardrobe"],
       dropProposition: "at",
     },
     inn: {
-      description: `You enter what appears to be the common room of an inn. ${itemLocations.inn.has("apple")
-        ? "A complementary apple rests on the table. "
-        : ""
-        }${gameState.naked
+      description: `You enter what appears to be the common room of an inn. ${
+        itemLocations.inn.has("apple")
+          ? "A complementary apple rests on the table. "
+          : ""
+      }${
+        gameState.naked
           ? 'The inn keeper laughs, "Haven\'t you heard of clothes?!"'
           : ""
-        }`,
+      }`,
       connections: ["room", "courtyard"],
       dropProposition: "in",
-      // If you are naked, lose reputation when you move here
       ...(gameState.naked && {
         onEnterGameStateEffect: { reputation: gameState.reputation - 1 },
       }),
     },
     courtyard: {
-      description: `You are in a small courtyard. The entrance to the inn sits at the north side. To the east you hear sounds of a blacksmith shop. To the west you see a fountain. ${gameState.fire ? "Beyond the fountain, you see flames and smoke. " : ""
-        }${gameState.firstCourtyardEntry
+      description: `You are in a small courtyard. The entrance to the inn sits at the north side. To the east you hear sounds of a blacksmith shop. To the west you see a fountain. ${
+        gameState.fire ? "Beyond the fountain, you see flames and smoke. " : ""
+      }${
+        gameState.firstCourtyardEntry
           ? "An adolescent runs west to east, crying as they flee. They drop a handkerchief in their distress. "
           : ""
-        }`,
+      }`,
       connections: ["inn", "fountain", "smithy"],
       dropProposition: "in",
       ...(gameState.firstCourtyardEntry && {
         onExitGameStateEffect: { firstCourtyardEntry: false },
-      })
+      }),
     },
     fountain: {
       connections: ["manor", "courtyard"],
       dropProposition: "in",
-      description: `You stand at the edge of a fountain. In the center is a statue of a dragon surrounded by cowering people. To the east is a courtyard. To the north is a manor. ${gameState.fire
-        ? "The manor is on fire and surrounded by a crowd of people. "
-        : "The manor is a framework of charred wood."
-        }${itemLocations.nursery.has("baby")
+      description: `You stand at the edge of a fountain. In the center is a statue of a dragon surrounded by cowering people. To the east is a courtyard. To the north is a manor. ${
+        gameState.fire
+          ? "The manor is on fire and surrounded by a crowd of people. "
+          : "The manor is a framework of charred wood."
+      }${
+        itemLocations.nursery.has("baby")
           ? 'You hear a voice sobbing, "My baby! My baby is trapped in the nursery."'
           : ""
-        }${(gameState.savedBaby && gameState.babyCough && !gameState.receivedBabyReward)
+      }${
+        gameState.savedBaby &&
+        gameState.babyCough &&
+        !gameState.receivedBabyReward
           ? 'You hear a voice: "My baby! You saved my baby! But my dear baby has a terrible cough from being carried through the smoke. Regardless, take this gold as thanks." As you take the gold and praise, you see the roof collapse. Finally, the crowd is able to douse the flames. '
           : ""
-        }${(gameState.savedBaby && !gameState.babyCough && !gameState.receivedBabyReward)
+      }${
+        gameState.savedBaby &&
+        !gameState.babyCough &&
+        !gameState.receivedBabyReward
           ? 'You hear a voice: "Thank you for saving my baby! Please take this gold as thanks." As you take the gold and praise, you see the roof collapse. Finally, the crowd is able to douse the flames.'
           : ""
-        }`,
-      ...((gameState.savedBaby && !gameState.receivedBabyReward) && {
-        onExitGameStateEffect: { fire: false, receivedBabyReward: true },
-        onEnterItemLocationEffect: { item: "baby", oldLocation: "inventory", newLocation: "outOfPlay" }
-      }),
-      ...((gameState.savedBaby && gameState.babyCough && !gameState.receivedBabyReward) && {
-        onEnterGameStateEffect: { gold: gameState.gold + 50, reputation: gameState.reputation + 1 },
-      }),
-      ...((gameState.savedBaby && !gameState.babyCough && !gameState.receivedBabyReward) && {
-        onEnterGameStateEffect: { gold: gameState.gold + 50, reputation: gameState.reputation + 2 },
-      }),
-
+      }`,
+      // todo could use consequence instead of on enter and on exit
+      // ...((gameState.savedBaby && !gameState.receivedBabyReward) && {
+      //   consequenceText: "",
+      //   consequenceStateEffect: {
+      //     fire: false,
+      //     receivedBabyReward: true
+      //   },
+      //   consequenceItemLocationEffect: {}
+      // }),
+      ...(gameState.savedBaby &&
+        !gameState.receivedBabyReward && {
+          onExitGameStateEffect: { fire: false, receivedBabyReward: true },
+          onEnterItemLocationEffect: {
+            item: "baby",
+            oldLocation: "inventory",
+            newLocation: "outOfPlay",
+          },
+        }),
+      ...(gameState.savedBaby &&
+        !gameState.receivedBabyReward && {
+          onEnterGameStateEffect: {
+            gold: gameState.gold + 50,
+            reputation: gameState.babyCough
+              ? gameState.reputation + 1
+              : gameState.reputation + 2,
+          },
+        }),
     },
-
-
+    manor: {
+      connections: ["fountain"], //todo could instead allow to continue if not masked but develop cough/lose reputation. todo could instead allow to continue if no fire but have manor collapse
+      ...(gameState.fire &&
+        gameState.handkerchiefDamp &&
+        gameState.masked && { connections: ["fountain", "nursery"] }),
+      dropProposition: "in",
+      description: `${
+        gameState.fire
+          ? "You stand in the entrance of the burning manor. "
+          : "You stand in the charred remains of the manor. "
+      }${
+        itemLocations.nursery.has("baby")
+          ? "You hear a baby crying upstairs. "
+          : ""
+      }${
+        gameState.fire && !gameState.handkerchiefDamp && !gameState.masked
+          ? "Your throat burns from the smoke and heat. You can't breath this air. "
+          : ""
+      }${
+        gameState.fire && gameState.handkerchiefDamp && gameState.masked
+          ? "Although the smoke is thick, the damp handkerchief over your mouth helps you breath."
+          : ""
+      }`,
+      ...(itemLocations.inventory.has("baby") && {
+        onEnterGameStateEffect: { babyCough: true },
+      }),
+      // ...((gameState.fire && !gameState.handkerchiefDamp && !gameState.masked) && {
+      //   consequenceText: "Your throat burns from the smoke and heat. You can't breath this air. You retreat back outside",
+      //   consequencePlayerLocationEffect: "fountain",
+      // }),
+    },
+    nursery: {
+      connections: ["nurseryWindow", "manor"],
+      dropProposition: "in",
+      description: `${
+        gameState.fire && itemLocations.nursery.has("baby")
+          ? "You stand in a nursery. You see a baby wailing in the crib under an open window. The open window must be the only thing keeping the baby alive in this smoke. "
+          : ""
+      }${
+        gameState.fire && !itemLocations.nursery.has("baby")
+          ? "You stand in a nursery with an empty crib. The fire continues to burn, pouring smoke into the room. "
+          : ""
+      }${
+        !gameState.fire ? "You stand in the charred remains of a nursery." : ""
+      }`,
+    },
+    nurseryWindow: {
+      connections: ["nursery"],
+      dropProposition: "at", // todo could change to out and have anything dropped out any window end in location below
+      description: `${
+        gameState.fire
+          ? "Below the window, you see the gathered crowd. "
+          : "You see the charred remains of the manor below you. "
+      }`,
+    },
   };
 
   const allItems = {
     lute: {
       spawnLocation: "room",
       description: "A wooden lute",
-      ...(playerLocation === "inn" && {
-        takeDescription: "The lute feels familiar",
-        takeLocation: "inn",
+      ...(playerLocation === "room" && {
+        takeDescription: "The lute feels familiar.",
       }),
-      ...(false && { takeDescription: "falsy" }),
       useVerb: "Play",
       useDescription: "You play a beautiful melody.",
     },
@@ -159,15 +247,15 @@ function App() {
       description: "A set of clothes",
       ...(gameState.naked
         ? {
-          useVerb: "Wear",
-          useDescription: "You put on the clothes.",
-          useSideEffect: { naked: false },
-        }
+            useVerb: "Wear",
+            useDescription: "You put on the clothes.",
+            useGameStateEffect: { naked: false },
+          }
         : {
-          useVerb: "Remove",
-          useDescription: "You strip down.",
-          useSideEffect: { naked: true },
-        }),
+            useVerb: "Remove",
+            useDescription: "You strip down.",
+            useGameStateEffect: { naked: true },
+          }),
     },
     apple: {
       spawnLocation: "inn",
@@ -179,30 +267,49 @@ function App() {
       spawnLocation: "adolescent",
       ...(gameState.handkerchiefDamp
         ? {
-          description: "A damp handkerchief",
-        }
+            description: "A damp handkerchief",
+          }
         : {
-          description: "A handkerchief",
-        }),
+            description: "A handkerchief",
+          }),
       ...(gameState.masked
         ? {
-          useVerb: "Remove",
-          useDescription:
-            "You remove the handkerchief from your nose and mouth.",
-          useSideEffect: { masked: false },
-        }
+            useVerb: "Remove",
+            useDescription:
+              "You remove the handkerchief from your nose and mouth.",
+            useGameStateEffect: { masked: false },
+          }
         : {
-          useVerb: "Wear",
-          useDescription:
-            "You tie the handkerchief around your nose and mouth.",
-          useSideEffect: { masked: true },
-        }),
+            useVerb: "Wear",
+            useDescription:
+              "You tie the handkerchief around your nose and mouth.",
+            useGameStateEffect: { masked: true },
+          }),
+      ...(playerLocation === "fountain" && {
+        dropGameStateEffect: { handkerchiefDamp: true },
+      }),
     },
     baby: {
       spawnLocation: "nursery",
       description: "A crying baby",
       useVerb: "Use",
-      useDescription: "It's unclear what use this item has.",
+      useDescription: "It's unclear what use this item has. ",
+      dropDescription: "You drop the crying baby. It cries even louder. ",
+      ...(playerLocation === "nursery" && {
+        takeDescription:
+          "You pick up the baby from the crib. The baby coughs as you move it away from the open window. ",
+        dropDescription: "You place the baby back in the crib. ",
+      }),
+      takeGameStateEffect: { savedBaby: true },
+      ...(!playerLocation === "nurseryWindow" && {
+        dropGameStateEffect: { savedBaby: false },
+      }),
+
+      ...(playerLocation === "nurseryWindow" && {
+        dropDescription:
+          "You drop the baby out of the open window. The crowd below catches the baby. ",
+        dropLocation: "outOfPlay",
+      }),
     },
     milk: {
       spawnLocation: "inn",
@@ -228,16 +335,20 @@ function App() {
 
   // buildStartingLocations();
 
+  function giveConsequence({ description }) {}
+
   function moveItem({ item, oldLocation, newLocation }) {
+    console.log(`'moving' ${item} from ${oldLocation} to ${newLocation}`);
     itemLocations[oldLocation].delete(item);
     itemLocations[newLocation].add(item);
     setItemLocations(itemLocations);
   }
 
   function handleMovePlayer(newLocation) {
-    let gameStateChanges = {}
+    let gameStateChanges = {};
 
     const oldLocation = playerLocation;
+
     if (locations[oldLocation].onExitGameStateEffect) {
       gameStateChanges = {
         ...gameStateChanges,
@@ -263,10 +374,15 @@ function App() {
       moveItem(locations[newLocation].onEnterItemLocationEffect);
     }
 
+    if (locations[oldLocation].onExitItemLocationEffect) {
+      moveItem(locations[oldLocation].onExitItemLocationEffect);
+    }
+
     setPlayerLocation(newLocation);
   }
 
   function handleTake(item) {
+    console.log(`handkerchief is damp ${gameState.handkerchiefDamp}`);
     // Get the "take"" description for the item -- this will be the consequence text
     const description = allItems[item].takeDescription
       ? allItems[item].takeDescription
@@ -276,6 +392,10 @@ function App() {
     const endItemLocation = allItems[item].takeLocation
       ? allItems[item].takeLocation
       : "inventory";
+
+    if (allItems[item].takeGameStateEffect) {
+      setGameState({ ...gameState, ...allItems[item].takeGameStateEffect });
+    }
 
     // Set the item location to the take end location
     moveItem({
@@ -296,11 +416,11 @@ function App() {
       ? allItems[item].useDescription
       : `You use the ${item}.`;
 
-    if (allItems[item].useSideEffect) {
-      setGameState({ ...gameState, ...allItems[item].useSideEffect });
+    if (allItems[item].useGameStateEffect) {
+      setGameState({ ...gameState, ...allItems[item].useGameStateEffect });
     }
 
-    // set the consequence text to the take description text
+    // set the consequence text to the use description text
     setConsequenceText(description);
 
     // set show consequence to true
@@ -318,14 +438,20 @@ function App() {
       ? allItems[item].dropLocation
       : playerLocation;
 
-    if (allItems[item].dropSideEffect) {
-      setGameState({ ...gameState, ...allItems[item].dropSideEffect });
+    console.log(`drop at ${endItemLocation}`);
+
+    if (allItems[item].dropGameStateEffect) {
+      setGameState({ ...gameState, ...allItems[item].dropGameStateEffect });
     }
 
     // Set the item location from the inventory to the new location
-    moveItem(item, "inventory", endItemLocation);
+    moveItem({
+      item: item,
+      oldLocation: "inventory",
+      newLocation: endItemLocation,
+    });
 
-    // set the consequence text to the take description text
+    // set the consequence text to the drop description text
     setConsequenceText(description);
 
     // set show consequence to true
